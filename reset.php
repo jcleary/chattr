58c20955ed7f608a0b70ae2f5559ec98595ba532
John Cleary <john@createk.io>
2016-08-23 11:20:59 +0100

    delete content from Redis on reset

--- reset.php
+++ reset.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('lib/database.php');
+require_once('lib/app.php');
 
 $commands = [
     'DROP DATABASE IF EXISTS chattr',
@@ -8,6 +8,9 @@ $commands = [
     'CREATE TABLE chattr.surveys (id INT NOT NULL AUTO_INCREMENT, name varchar(255), PRIMARY KEY (id))',
     'CREATE TABLE chattr.survey_options (id INT NOT NULL AUTO_INCREMENT, survey_id INT NOT NULL, value varchar(255), PRIMARY KEY (id))'
 ];
+
+global $redis;
+$redis->flushAll();
 
 db_exec($commands);
 
